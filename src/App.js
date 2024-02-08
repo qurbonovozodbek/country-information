ed421027f910a9a3990d6b72f22aa84c2daa241a
qurbonovozodbek <ozodbekkurbonov185@gmail.com>
2024-02-08 18:36:34 +0500

    info-county

---
+++ src/App.js
@@ -0,0 +1,100 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div class="container">
+        <div class="card">
+            <img src="https://external-preview.redd.it/o9maPTgCnKD7ciDf0xhHZUVGi_qsKSXFYvVcVFyGoIM.png?auto=webp&s=c40b526af97e31354204b2dec0bf74fd82a7d7e3" alt=""/>
+            <div class="info">
+                <h2><span>name: </span>Germany</h2>
+                <h4><span>population: </span>76456454</h4>
+                <h4><span>language: </span>nemis</h4>
+            </div>
+        </div>
+
+        <div class="card">
+            <img src="https://clipground.com/images/italy-flag-png-7.png" alt=""/>
+            <div class="info">
+                <h2><span>name: </span>Italy</h2>
+                <h4><span>population: </span>81234443</h4>
+                <h4><span>language: </span>italy</h4>
+            </div>
+        </div>
+
+        <div class="card">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/6/62/Flag_of_France.png" alt=""/>
+            <div class="info">
+                <h2><span>name: </span>France</h2>
+                <h4><span>population: </span>2342398</h4>
+                <h4><span>language: </span>Franch</h4>
+            </div>
+        </div>
+
+        <div class="card">
+            <img src="https://th.bing.com/th/id/R.9d7997e4e13b1c737217f8d1a3f1c6f5?rik=2tCsgtAE5w6xoA&riu=http%3a%2f%2fechesters.co.uk%2fimages%2fposts%2fflag-of-spain.png&ehk=t0cUSAxXePoXOokg%2bPWqCFoxqJQ%2bXhrIA3V%2b9QQEeoM%3d&risl=&pid=ImgRaw&r=0" alt=""/>
+            <div class="info">
+                <h2><span>name: </span>Spain</h2>
+                <h4><span>population: </span>59486794</h4>
+                <h4><span>language: </span>Span</h4>
+            </div>
+        </div>
+
+        <div class="card">
+            <img src="https://th.bing.com/th/id/R.c57ce4a915d632d05636855d0c5cb348?rik=JNC%2bdDZ3M%2fW3GA&riu=http%3a%2f%2fs1.bwallpapers.com%2fwallpapers%2f2014%2f05%2f29%2fuzbekistan-flag_121436716.jpg&ehk=y6oEgEPQG53YM6ZVHcdWwiR8Too3GWfvepgBz8D5BQ0%3d&risl=&pid=ImgRaw&r=0" alt=""/>
+            <div class="info">
+                <h2><span>name: </span>Uzbekistan</h2>
+                <h4><span>population: </span>35678876</h4>
+                <h4><span>language: </span>Lotin</h4>
+            </div>
+        </div>
+
+        <div class="card">
+            <img src="https://www.publicdomainpictures.net/pictures/250000/velka/russian-flag-151947920021T.jpg" alt=""/>
+            <div class="info">
+                <h2><span>name: </span>Russia</h2>
+                <h4><span>population: </span>165098788</h4>
+                <h4><span>language: </span>Rus</h4>
+            </div>
+        </div>
+
+        <div class="card">
+            <img src="https://th.bing.com/th/id/R.8ddd9500c1c4710fba5d03e389745454?rik=1hZqsxoK%2b5WCZw&riu=http%3a%2f%2fupload.wikimedia.org%2fwikipedia%2fcommons%2fd%2fde%2fFlag_of_the_United_States.png&ehk=UQNcwD%2f6s4YxGw5K0hT8nGmeZYRbCoevgtAonR%2bchjY%3d&risl=&pid=ImgRaw&r=0" alt=""/>
+            <div class="info">
+                <h2><span>name: </span>United States</h2>
+                <h4><span>population: </span>56794867459</h4>
+                <h4><span>language: </span>English</h4>
+            </div>
+        </div>
+
+        <div class="card">
+            <img src="https://th.bing.com/th/id/R.65723de60e43ee8c130228e4e1c7eaf3?rik=hVCD660z3J4bHA&riu=http%3a%2f%2fwonderfulengineering.com%2fwp-content%2fuploads%2f2015%2f07%2fArgentina-flag-6.jpg&ehk=zy%2bGbSVbhj67JfY6RG2tFvV2%2fgv7NT4fzqmob1cw%2bsE%3d&risl=&pid=ImgRaw&r=0" alt=""/>
+            <div class="info">
+                <h2><span>name: </span>Argentina</h2>
+                <h4><span>population: </span>9867565</h4>
+                <h4><span>language: </span>Argentian</h4>
+            </div>
+        </div>
+
+        <div class="card">
+            <img src="https://th.bing.com/th/id/R.a2390f2d4f8186a7df245d5f6ecee8eb?rik=eF6wWWxM4L6zxw&riu=http%3a%2f%2fflagpedia.net%2fdata%2fflags%2fultra%2fpt.png&ehk=SIso3jIhTH3dSD9piuI9ookQy4%2fjGO3V%2fqZ15LQuOvU%3d&risl=&pid=ImgRaw&r=0" alt=""/>
+            <div class="info">
+                <h2><span>name: </span>Portugalia</h2>
+                <h4><span>population: </span>9876855</h4>
+                <h4><span>language: </span>Portugal</h4>
+            </div>
+        </div>
+
+        <div class="card">
+            <img src="https://i.pinimg.com/736x/7d/5b/20/7d5b204de1a2fd5dda103a1745a64c74.jpg" alt=""/>
+            <div class="info">
+                <h2><span>name: </span>South KOrea</h2>
+                <h4><span>population: </span>4674594</h4>
+                <h4><span>language: </span>언어</h4>
+            </div>
+        </div>
+    </div>
+  );
+}
+
+export default App;
